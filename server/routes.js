2d0df40208955223238157204579c788e8967187
nanadarmesh <darmesh.nana@gmail.com>
2020-11-24 10:05:03 +0100

    asdf;

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,19 @@
 let express = require('express')
 let router  = express.Router()
+
+let xml = require('xml');
+let name_of_restaurants = [
+    {f1: "1"}
+]
+
 // let passport    = require('passport');
 // let xsenv       = require('@sap/xsenv');
 // let JWTStrategy = require('@sap/xssec').JWTStrategy;
 
-router.get('/backend', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end("Backend has been reached\n")
+router.get('/Experimental/OData/OData.svc', function(req, res) {
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.end("Backend has been reached\n")
+    res.set('Content-Type', 'text/xml');
+    res.send(xml(name_of_restaurants));
 })
 module.exports = router
